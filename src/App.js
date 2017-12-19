0837a66982055144b2e75c320b36c05da1b55ce9
Thomas Le <le.thomas.vu@gmail.com>
2017-12-18 18:29:40 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import uuid from 'uuid';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: [],
+    }
+  }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
+
+  handleDeleteProject(id){
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects:projects});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AddProject addProject={this.handleAddProject.bind(this)} />
+        <br />
+        <hr />
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default App;
